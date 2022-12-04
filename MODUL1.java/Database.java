69a032e363bb815c59b306714c4504937326629c
revandaoctavian <115240965+revandaoctavian@users.noreply.github.com>
2022-12-04 19:07:32 +0700

    Add files via upload

---
+++ MODUL1.java/Database.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database {
+     
+    List<Menu>listMenu = new ArrayList<Menu>();
+
+    public void insertMenu(Menu menu) {
+        listMenu.add(menu);
+    }
+
+    public void showMenu(Menu menu) {
+        
+    }
+
+    public void search(Menu menu) {
+
+    }
+    // TODO Create Method to Insert Menu to Database
+ 
+    // TODO Create Method to Show Menu from Database
+
+    // TODO Create Method to Search Menu from Database
+
+
+}
---
+++ MODUL1.java/User.java
@@ -0,0 +1,18 @@
+public class User {
+
+    String name;
+    String phoneNumber;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
