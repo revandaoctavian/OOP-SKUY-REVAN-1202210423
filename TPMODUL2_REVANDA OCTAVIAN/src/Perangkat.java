32611ee9b8086153395ad8fa6edba2000a3a9edb
Revanda Octavian <115240965+revandaoctavian@users.noreply.github.com>
2022-12-04 21:00:08 +0700

    Add files via upload

---
+++ TPMODUL2_REVANDA OCTAVIAN/src/Perangkat.java
@@ -0,0 +1,19 @@
+public class Perangkat {
+
+    String drive;
+    int ram;
+    float processor;
+
+    Perangkat(String drive, int ram, float processor) {
+        this.drive = drive;
+        this.ram = ram;
+        this.processor = processor;
+    }
+
+    public void informasi() {
+        System.out.printf(
+                "Perangkat tidak diketahui memiliki drive tipe Adata dengan ram sebesar %d GB dan processor secepat %.2f Ghz \n",
+                this.ram, this.processor);
+    }
+
+}
